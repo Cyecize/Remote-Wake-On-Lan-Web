3c47ff921bd537b7f969cedd3504a74ba4d0b4bc
Tsvetelin Yakimov <ceci2205@abv.bg>
2022-07-26 17:58:44 +0300

    Initial commit: Add project skeleton

---
+++ src/main/java/com/cyecize/wakeonlan/WakeOnLanApplication.java
@@ -0,0 +1,13 @@
+package com.cyecize.wakeonlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakeOnLanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WakeOnLanApplication.class, args);
+    }
+
+}
